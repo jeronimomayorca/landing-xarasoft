8798fa74e7520052eb84d5cba879675a2fb5fbe6
JERONIMO MAYORCA <mayorcajeronimo@gmail.com>
2024-11-15 00:59:58 -0500

    refactor header and fix colors ono light/dark schema

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config'
+import react from '@astrojs/react'
 import mdx from '@astrojs/mdx'
 import tailwind from '@astrojs/tailwind'
 import compress from 'astro-compress'
 import icon from "astro-icon"
 
+
 // https://astro.build/config
 export default defineConfig({
   compressHTML: true,
-  integrations: [mdx(), icon(), tailwind({
+  integrations: [react(), mdx(), icon(), tailwind({
     applyBaseStyles: false,
   }), compress()],
 })
